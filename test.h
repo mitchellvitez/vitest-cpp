f806d29a27454de73212548419ce2e5f702170a6
Mitchell Vitez <mitchell@vitez.me>
2014-11-21 01:30:19 -0500

    Added unittest(), which is run in random order and outside other functions. Added namespace vitest. Added message() and here().

--- test.h
+++ test.h
@@ -1,20 +1,37 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *	Improving on assert() in ~50 lines of macros.                      *
- * By Mitchell Vitez. Feel free to reuse and modify with attribution.  *
- * Originally built for testing coding projects at umich.              *
- * Nov 20 2014 | mvitez@umich.edu | www.vitez.me                       *
+ * DESCRIPTION:                                                        *
+ *  Building on assert() in ~100 lines of macros.                      *
+ *                                                                     *
+ * AUTHOR:                                                             *
+ *  By Mitchell Vitez. Feel free to reuse and modify with attribution. *
+ *  Originally built for testing coding projects at umich.             *
+ *  Nov 20 2014 | mvitez@umich.edu | www.vitez.me                      *
  *                                                                     *
  * USAGE: use precondition() at the very beginning of a function to    *
  *  check whether your assumptions are correct.                        *
  *  Likewise with postcondition(), except used right before returning  *
  *  at the end of a function.                                          *
  *                                                                     *
- *  test() does not exit the program. This is to encourage test-   *
+ *  test() does not exit the program. This is to encourage test-       *
  *  driven development without inducing frustration.                   *
  *                                                                     *
  *  Unlike with assert(), it is possible to pass an optional message   *
  *  along with your conditional so reading output you know exactly     *
  *  what you were trying to accomplish                                 *
+ *                                                                     *
+ *  unittest() lets you put several test() statements inside it. These *
+ *  will be run in a randomized order as the first thing in your       *
+ *  program if you call runUnitTests(); as the first line in main.     *
+ *  They are useful for separating testing out from running code, as   *
+ *  the unittest() macro doesn't need to be inside a function          *
+ *                                                                     *
+ *  unittest() can take arbitrary statements as well, if you need to   *
+ *  set up tests (constructors, etc.)                                  *
+ *                                                                     *
+ *	message(string msg) outputs a message unconditionally              *
+ *                                                                     *
+ *  here() outputs if the program gets to a certain point, since I     *
+ *  used to write a lot of cout << "function name got to here";        *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 #include <iostream>
 #include <stdlib.h> 
@@ -24,6 +41,45 @@ using namespace std;
 
 #ifndef TEST_H
 #define TEST_H
+
+/////// Above this line doesn't need functional, algorithm, vector ///////
+
+#include <vector>
+#include <functional>
+#include <algorithm>
+
+namespace vitest {
+
+#define concat(a, b) a##b
+#define concatenate(a, b) concat(a, b)
+#define unittest( ... )  static int concatenate(unittest, __LINE__) = \
+	addUnitTest([](){ __VA_ARGS__ }); 
+
+vector<function<void()>> unitTests;
+
+int addUnitTest(function<void()> func) {
+	#ifndef NDEBUG
+		unitTests.push_back(func);
+		return 1;
+	#endif
+}
+
+void runUnitTests() {
+	#ifndef NDEBUG
+		// Random seed, to help run unit tests in random order
+		srand(unsigned(time(NULL)));
+		// Ensures they are independent unit tests
+		random_shuffle(unitTests.begin(), unitTests.end());
+		for (auto unitTest = unitTests.begin(); 
+				unitTest != unitTests.end(); unitTest++)
+			(*unitTest)();
+	#endif
+}
+
+} // namespace vitest
+
+/////// Below this line doesn't need functional, algorithm, vector ///////
+namespace vitest {
 
 #define PRECONDITION "Precondition"
 #define POSTCONDITION "Postcondition"
@@ -68,6 +124,24 @@ void conditionTest(string condition, string message, string file,
 void conditionTest(string condition, string file, int line, 
 		string function, string type, bool shouldExit) {
 	conditionTest(condition, "", file, line, function, type, shouldExit);
+}
+
+#ifdef NDEBUG
+# define message( ... )
+# define here( ... )
+#else
+# define message( msg ) printMessage(msg, __FILE__, __LINE__, __PRETTY_FUNCTION__)
+# define here( ) printHere(__FILE__, __LINE__, __PRETTY_FUNCTION__)
+#endif
+
+void printMessage(string message, string file, int line, string function) {
+	cerr << file << ":" << line << ": " << function << ": Message: " 
+		<< message << "\n";
+}
+
+void printHere(string file, int line, string function) {
+	cerr << file << ":" << line << ": " << function << ": here\n";
+}
 }
 
 #endif
--- testrun.cpp
+++ testrun.cpp
@@ -1,23 +1,10 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *	Testing for test.h (It's turtles all the way down)                 *
- * By Mitchell Vitez. Feel free to reuse and modify with attribution.  *
- * Originally built for testing coding projects at umich.              *
- * Nov 20 2014 | mvitez@umich.edu | www.vitez.me                       *
- *                                                                     *
- * USAGE: use precondition() at the very beginning of a function to    *
- *  check whether your assumptions are correct.                        *
- *  Likewise with postcondition(), except used right before returning  *
- *  at the end of a function.                                          *
- *                                                                     *
- *  test() does not exit the program. This is to encourage test-   *
- *  driven development without inducing frustration.                   *
- *                                                                     *
- *  Unlike with assert(), it is possible to pass an optional message   *
- *  along with your conditional so reading output you know exactly     *
- *  what you were trying to accomplish                                 *
+ * Testing for test.h (It's turtles all the way down)                  *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 #include "test.h"
 #include <iostream>
+
+using namespace vitest;
 
 // Returns (n+1) squared
 int workingFunction(int n) {
@@ -25,7 +12,7 @@ int workingFunction(int n) {
 	precondition(n != 0, "n should be nonzero");
 	precondition(n > -10000);
 	n++;
-	x = 0;
+	int x = 0;
 	n = n * n;
 	postcondition(n < 10000);
 	postcondition(n != 1, "n shouldn't be one at this point");
@@ -33,6 +20,12 @@ int workingFunction(int n) {
 	postcondition(x == 0);
 	return n;
 }
+unittest(
+	test(workingFunction(5) == 36, "(5 + 1) ^ 2 is 36");
+	test(workingFunction(6) == 49, "(6 + 1) ^ 2 is 49");
+	test(workingFunction(-11) == 100, "(-11 + 1) ^ 2 is 100");
+	test(workingFunction(-11) == 89, "10 ^ 2 == 89");
+)
 
 // Tries to return (n+1) squared, but fails
 int brokenFunction(int n) {
@@ -41,11 +34,28 @@ int brokenFunction(int n) {
 		return n * n;
 	return n * n + 1;
 }
+unittest(
+	cout << "unittest 2\n";
+)
 
 void runNumberTests() {
 	test(5 == 8, "Testing if numbers work");
 	test(2 == 2, "You won't see this");
 }
+
+// Testing since unittest should run in random order--it does
+unittest(
+	cout << "unittest 3\n";
+)
+unittest(
+	cout << "unittest 4\n";
+)
+unittest(
+	cout << "unittest 5\n";
+)
+unittest(
+	cout << "unittest 6\n";
+)
 
 void runWorkingTests() {
 	test(workingFunction(7) == 64);
@@ -73,7 +83,25 @@ void runOtherTests() {
 	std::cout << "Yay, no errors!" << std::endl;
 }
 
+void hereTest() {
+	int x = 3;
+	here();
+	x--;
+	x--;
+	cout << "liftoff\n";
+}
+
+void messageTest() {
+	int y = 100, k = 10;
+	message("things are going well");
+	y * 2 * k; // y2k == 2000
+}
+
 int main() {
+	runUnitTests();
+
+	hereTest();
+	messageTest();
 
 #ifndef NDEBUG
 	runNumberTests();
@@ -82,5 +110,5 @@ int main() {
 	runOtherTests();
 #endif
 
-	cout << "Non debugging code" << endl;
+	cout << "other code here" << endl;
 }
